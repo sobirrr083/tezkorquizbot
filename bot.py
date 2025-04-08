f518028d4e1da4dc613cc386de612a900f86f363
sobirrr083 <161307835+sobirrr083@users.noreply.github.com>
2025-04-08 18:37:44 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
 import os
 import openai
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import start_polling
+from aiogram import executor  
 from dotenv import load_dotenv
-
 
 load_dotenv()
 BOT_TOKEN = os.getenv("8175665332:AAH8Zbtj7Mbxau_BKspKdeDvGHParj_ewXA")  
-OPENAI_API_KEY = os.getenv("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")
+OPENAI_API_KEY = os.getenv("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")  
+
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 openai.api_key = OPENAI_API_KEY
-
 
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
@@ -31,6 +30,5 @@ async def chat_with_ai(message: types.Message):
     )
     await message.reply(response["choices"][0]["message"]["content"])
 
-# Botni ishga tushirish
 if __name__ == "__main__":
-    start_polling(dp, skip_updates=True)  # Yangi usulda botni ishga tushirish
+    executor.start_polling(dp, skip_updates=True)  
